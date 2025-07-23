857230d6cc6676e9fbfce3144fba0d3149fdabdc
Siva7007 <sivaramansvv@gmail.com>
2025-07-23 10:39:42 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_OIOMB9Ro1', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '59og6i64dvfl819c7973dseilp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_Gomoj78OC', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '51q49enae8jg61pbneet08mfre', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
